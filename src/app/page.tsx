947459f6ca2de0ef87165459b1d6e7b1a122b356
Gustavo Sales <gustavo@gsales.io>
2023-06-17 15:32:33 -0300

    feat: removing what is not necessary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import RandomImage from '@/components/random-image';
-import getRandomImageData from '@/services/get-random-image-data';
-import { faker } from '@faker-js/faker';
-
 export default async function Home() {
-  const initialImageData = await getRandomImageData({
-    word: faker.animal.dog(),
-  });
-
   return (
     <main className="flex min-h-screen flex-col items-center gap-6 p-24">
       <h1 className="text-4xl font-bold">Random Image App</h1>
-
-      <RandomImage initialImageData={initialImageData} />
     </main>
   );
 }
